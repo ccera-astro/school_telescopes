07a1e1aa29c2b7bb4fd5b35ee57399479ebbc95a
Marcus D. Leech <mleech@lab.localdomain>
2018-04-13 15:33:50 -0400

    Radiometer needed to do the rename dance differently.
    Needed to turn off caching for static file handler.

--- astro_web.py
+++ astro_web.py
@@ -84,6 +84,9 @@ class BaseHandler(tornado.web.RequestHandler):
 
 
 class Handler(tornado.web.StaticFileHandler):
+    def set_extra_headers(self, path):
+        # Disable cache
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
     def parse_url_path(self, url_path):
         if not url_path or url_path.endswith('/'):
             url_path = url_path + 'index.html'
